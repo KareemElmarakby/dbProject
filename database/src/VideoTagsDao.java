8cc0d9c4a2d3a10dfc655c43926af75faad94fbd
jbucciero <65266196+jbucciero@users.noreply.github.com>
2020-07-27 22:41:13 -0400

    fixed my merge to hopefully retrive the items

--- database/src/VideoTagsDao.java
+++ database/src/VideoTagsDao.java
@@ -59,18 +59,6 @@ public class VideoTagsDao {
                        " tag VARCHAR(50), " + 
                        " PRIMARY KEY (url, tag)) "; 
         
-<<<<<<< HEAD
-        String sql3 = "insert into VideoTags(url, tag) values (\"youtube.com/1\", \"cool\")";
-        String sql4 = "insert into VideoTags(url, tag) values (\"youtube.com/2\", \"cool\")";
-        String sql5 = "insert into VideoTags(url, tag) values (\"youtube.com/3\", \"cool\")";
-        String sql6 = "insert into VideoTags(url, tag) values (\"youtube.com/4\", \"cool\")";
-        String sql7 = "insert into VideoTags(url, tag) values (\"youtube.com/5\", \"cool\")";
-        String sql8 = "insert into VideoTags(url, tag) values (\"youtube.com/6\", \"cool\")";
-        String sql9 = "insert into VideoTags(url, tag) values (\"youtube.com/7\", \"cool\")";
-        String sql10 = "insert into VideoTags(url, tag) values (\"youtube.com/8\", \"cool\")";
-        String sql11 = "insert into VideoTags(url, tag) values (\"youtube.com/9\", \"cool\")";
-        String sql12 = "insert into VideoTags(url, tag) values (\"youtube.com/10\", \"cool\")";
-=======
         String sql3 = "insert into VideoTags(url, tag) values (\"youtube.com/1\", \"cool,fun,tables!\")";
         String sql4 = "insert into VideoTags(url, tag) values (\"youtube.com/2\", \"sick,neat,tables!\")";
         String sql5 = "insert into VideoTags(url, tag) values (\"youtube.com/3\", \"sick,fun,tables!\")";
@@ -81,7 +69,7 @@ public class VideoTagsDao {
         String sql10 = "insert into VideoTags(url, tag) values (\"youtube.com/8\", \"sick,neat,tables!\")";
         String sql11 = "insert into VideoTags(url, tag) values (\"youtube.com/9\", \"bad,dog,fun\")";
         String sql12 = "insert into VideoTags(url, tag) values (\"youtube.com/10\", \"school,students\")";
->>>>>>> branch 'master' of https://github.com/KareemElmarakby/dbProject.git
+
 
         try {
         	
